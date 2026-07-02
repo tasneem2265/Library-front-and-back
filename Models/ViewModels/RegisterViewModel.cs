464b350386105889788ad1c513f1ceb123243f18
tasneem2265 <tasneem2005saber@gmail.com>
2026-07-02 21:40:56 +0300

    Update RegisterViewModel.cs

--- Models/ViewModels/RegisterViewModel.cs
+++ Models/ViewModels/RegisterViewModel.cs
@@ -1,1 +1,26 @@
-d
+// Models/ViewModels/RegisterViewModel.cs
+public class RegisterViewModel
+{
+    [Required(ErrorMessage = "الاسم مطلوب")]
+    [StringLength(100)]
+    public string FullName { get; set; }
+
+    [Required(ErrorMessage = "البريد الإلكتروني مطلوب")]
+    [EmailAddress(ErrorMessage = "بريد إلكتروني غير صحيح")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "رقم الهاتف مطلوب")]
+    [Phone]
+    public string PhoneNumber { get; set; }
+
+    public string Address { get; set; }
+
+    [Required(ErrorMessage = "كلمة المرور مطلوبة")]
+    [StringLength(100, MinimumLength = 6, ErrorMessage = "كلمة المرور يجب أن تكون 6 أحرف على الأقل")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "كلمة المرور غير متطابقة")]
+    public string ConfirmPassword { get; set; }
+}
