7d17df7181a092c69fafa169e04af8d0dfbc67b5
tasneem2265 <tasneem2005saber@gmail.com>
2026-07-02 19:24:47 +0300

    Update Cart.cs

--- Models/Cart.cs
+++ Models/Cart.cs
@@ -1,1 +1,10 @@
-t
+// Models/Cart.cs
+public class Cart
+{
+    public int Id { get; set; }
+    
+    public string ApplicationUserId { get; set; }
+    public ApplicationUser ApplicationUser { get; set; }
+
+    public ICollection<CartItem> CartItems { get; set; } = new List<CartItem>();
+}
