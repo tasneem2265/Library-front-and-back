9b9e28c39311b881141cf1dd060f772c7f0b847d
tasneem2265 <tasneem2005saber@gmail.com>
2026-07-02 19:27:00 +0300

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,1 +1,26 @@
-f
+// Controllers/HomeController.cs
+public class HomeController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public HomeController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var latestBooks = await _context.Books
+            .Include(b => b.BookCategories)
+                .ThenInclude(bc => bc.Category)
+            .Where(b => b.IsAvailable)
+            .OrderByDescending(b => b.CreatedAt)
+            .Take(8)
+            .ToListAsync();
+
+        var categories = await _context.Categories.ToListAsync();
+
+        ViewBag.Categories = categories;
+        return View(latestBooks);
+    }
+}
