fe149ed444f45b85a142670b0c38d6be5face5c3
tasneem2265 <tasneem2005saber@gmail.com>
2026-07-02 19:25:34 +0300

    Update OrderItem.cs

--- Models/OrderItem.cs
+++ Models/OrderItem.cs
@@ -1,1 +1,16 @@
-c
+// Models/OrderItem.cs
+public class OrderItem
+{
+    public int Id { get; set; }
+    
+    public int OrderId { get; set; }
+    public Order Order { get; set; }
+    
+    public int BookId { get; set; }
+    public Book Book { get; set; }
+    
+    public int Quantity { get; set; }
+    
+    // سعر وقت الطلب (عشان لو السعر اتغير بعدين)
+    public decimal UnitPrice { get; set; }
+}
