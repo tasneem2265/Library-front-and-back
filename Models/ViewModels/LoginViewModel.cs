cb088f422f1445dc11bd8a7c2b7f203d13421ab8
tasneem2265 <tasneem2005saber@gmail.com>
2026-07-02 21:41:18 +0300

    Update LoginViewModel.cs

--- Models/ViewModels/LoginViewModel.cs
+++ Models/ViewModels/LoginViewModel.cs
@@ -1,1 +1,13 @@
-d
+// Models/ViewModels/LoginViewModel.cs
+public class LoginViewModel
+{
+    [Required(ErrorMessage = "البريد الإلكتروني مطلوب")]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "كلمة المرور مطلوبة")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    public bool RememberMe { get; set; }
+}
