4ac100ab2b0ad961586e71ba64b9d7936463e578
tasneem2265 <tasneem2005saber@gmail.com>
2026-07-02 19:23:57 +0300

    Update Category.cs

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,1 +1,12 @@
-c
+// Models/Category.cs
+public class Category
+{
+    public int Id { get; set; }
+    
+    [Required, StringLength(100)]
+    public string Name { get; set; }
+    
+    public string Description { get; set; }
+
+    public ICollection<BookCategory> BookCategories { get; set; } = new List<BookCategory>();
+}
