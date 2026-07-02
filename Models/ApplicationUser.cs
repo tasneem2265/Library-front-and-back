ccaef9ba5b6e69059937e96fa551b3ebaa22d665
tasneem2265 <tasneem2005saber@gmail.com>
2026-07-02 19:24:27 +0300

    Update ApplicationUser.cs

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -1,1 +1,11 @@
-t
+// Models/ApplicationUser.cs
+public class ApplicationUser : IdentityUser
+{
+    [StringLength(100)]
+    public string FullName { get; set; }
+    
+    public string Address { get; set; }
+    
+    public ICollection<Order> Orders { get; set; } = new List<Order>();
+    public Cart Cart { get; set; }
+}
