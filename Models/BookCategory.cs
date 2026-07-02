4cd4b6396563c20cbc2094540491956b4a885e64
tasneem2265 <tasneem2005saber@gmail.com>
2026-07-02 19:24:12 +0300

    Update BookCategory.cs

--- Models/BookCategory.cs
+++ Models/BookCategory.cs
@@ -1,1 +1,9 @@
-d
+// Models/BookCategory.cs (Many-to-Many join)
+public class BookCategory
+{
+    public int BookId { get; set; }
+    public Book Book { get; set; }
+    
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+}
