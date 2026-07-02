197c2f1c0da1d73f082fdac82972f1377cdde5e1
tasneem2265 <tasneem2005saber@gmail.com>
2026-07-02 21:12:10 +0300

    Update CheckoutViewModel.cs

--- Models/ViewModels/CheckoutViewModel.cs
+++ Models/ViewModels/CheckoutViewModel.cs
@@ -1,1 +1,14 @@
-f
+// Models/ViewModels/CheckoutViewModel.cs
+public class CheckoutViewModel
+{
+    public List<CartItem> CartItems { get; set; } = new();
+    public decimal Total { get; set; }
+
+    [Required(ErrorMessage = "من فضلك أدخلي عنوان الشحن")]
+    [StringLength(300)]
+    public string ShippingAddress { get; set; }
+
+    [Required(ErrorMessage = "من فضلك أدخلي رقم الهاتف")]
+    [Phone]
+    public string PhoneNumber { get; set; }
+}
